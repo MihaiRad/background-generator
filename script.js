a681a3a4ee83bae5b4ce41e88f7b3275e3056018
MihaiRad <mihai.rad94@gmail.com>
2022-04-17 15:26:41 +0300

    adding starting project

---
+++ script.js
@@ -0,0 +1,7 @@
+const flattened = [[0, 1], [2, 3], [4, 5]].reduce(
+    (accumulator, array) => {
+        // console.log('array', array);
+        // console.log('accumulator', accumulator);
+        debugger;
+        return accumulator.concat(array);
+    }, []);
